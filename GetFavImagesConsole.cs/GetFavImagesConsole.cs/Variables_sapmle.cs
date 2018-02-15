989a67e89620d03e8733f9396421833222d7308c
Fox Northern <postmaster@.club>
2018-02-15 23:16:25 +0900

    commt1

---
+++ GetFavImagesConsole.cs/GetFavImagesConsole.cs/Variables_sapmle.cs
@@ -0,0 +1,18 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GetFavImagesConsole.cs
+{
+    class Variables
+    {
+        // Insert your API key values here.
+        static readonly string consumerKey = "";
+        static readonly string consumerSecret = "";
+    }
+}
+*/
